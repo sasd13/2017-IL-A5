373eaba676adfc0dbbcdc68ae2a080edcf856417
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-04-14 10:00:43 +0200

    Fixed Norm2. Pearson must be implemented.

--- Algo/Algo.Reco/Reco/RecoContext.cs
+++ Algo/Algo.Reco/Reco/RecoContext.cs
@@ -20,18 +20,31 @@ namespace Algo
 
         public double DistNorm2( User u1, User u2 )
         {
-            var sum = u1.Ratings.Select(mr1 => new
+            var squareDeltas = u1.Ratings.Select(mr1 => new
                         {
                             R1 = mr1.Value,
                             R2 = u2.Ratings.GetValueWithDefault(mr1.Key, -1)
                         })
                         .Where(r1r2 => r1r2.R2 >= 0)
                         .Select(r1r2 => r1r2.R1 - r1r2.R2)
-                        .Select(delta => delta * delta)
-                        .Sum();
-            return Math.Sqrt( sum );
+                        .Select(delta => delta * delta);
+            // By considering the users as being the same, we boost
+            // the movies loved the most by all users.
+            return squareDeltas.Any()
+                    ? Math.Sqrt(squareDeltas.Sum())
+                    : 0.0;
         }
-    }
+
+        public double SimilarityNorm2(User u1, User u2)
+        {
+            return 1 / (1 + DistNorm2(u1, u2));
+        }
+
+        public double SimilarityPearson(User u1, User u2)
+        {
+            throw new NotImplementedException();
+        }
+   }
 
 
     public static class DictionaryExtension
