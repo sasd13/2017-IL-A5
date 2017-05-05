bdb0f77cbc2dd292ff3a56f4b51e026e57f62769
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-05-05 13:06:16 +0200

    Abstraction layer.

---
+++ Algo/Algo.Optim/SolutionSpace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Optim
+{
+    public abstract class SolutionSpace
+    {
+        readonly Random _random;
+
+        protected SolutionSpace(int seed )
+        {
+            _random = new Random(seed);
+        }
+
+        protected void Initialize(int[] cardinalities)
+        {
+            Cardinalities = cardinalities;
+        }
+
+        public int Dimension => Cardinalities.Length;
+
+        public int[] Cardinalities { get; private set; }
+
+        public SolutionInstance GetRandomInstance()
+        {
+            int[] coord = new int[Dimension];
+            for(int i = 0; i < Dimension; ++i )
+            {
+                coord[i] = _random.Next(Cardinalities[i]);
+            }
+            return CreateSolutionInstance(coord);
+        }
+
+        protected abstract SolutionInstance CreateSolutionInstance(int[] coord);
+    }
+}
